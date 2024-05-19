6bef440b64734ce8a25475195db82873a2aea324
Raafay Qureshi <79816835+Raafay-Qureshi@users.noreply.github.com>
2024-05-19 09:30:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: 'tL4no1r7QBdZHIwK9+Vk5VNDbjUEdjHhTmuOGSLGD6Z0pcV343OYO8t9rdayQNfKhAxEHWeh+ZEYZRCicyA/hA==',
   db: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   }
 };
